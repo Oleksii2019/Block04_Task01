8f46cdb11750971ac7f110afc57e0e2a7103bcbc
Oleksii2019 <Oleksii2019@users.noreply.github.com>
2019-06-04 10:03:44 +0300

    Начало

--- src/view/view.java
+++ src/view/view.java
@@ -1,4 +1,10 @@
 package view;
 
 public class view {
+    public static final String INPUT_DATA = "Your full name: ";
+
+    pnblic void printMessage(String mess) {
+        System.out.print(mess);
+    }
+    
 }
