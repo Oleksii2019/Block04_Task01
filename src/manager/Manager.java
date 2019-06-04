374aacbe5219e587f451807bd5d56942ef979df1
Oleksii2019 <Oleksii2019@users.noreply.github.com>
2019-06-04 11:29:40 +0300

    Написание 1

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,18 +1,34 @@
 package manager;
 
+import view.*;
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class Manager {
+    View view;
     Scanner scanner;
 
-    Manager() {
-        scanner = new Scanner();
-
+    public Manager(View view) {
+        scanner = new Scanner(System.in);
+        this.view = view;
     }
 
     private void stopScanner() {
         scanner.close();
     }
+
+    public void getName() {
+        view.printMessage(view.INPUT_DATA);
+        String regex = "^[0-9]+[^5]+$";
+        String Name = scanner.nextLine();
+        if (Name.matches(regex)) {
+            view.printMessage(view.OK_MESSAGE);
+        }
+        else
+            view.printMessage(view.NOT_OK_MESSAGE);
+        stopScanner();
+    }
+
 
 
 }
