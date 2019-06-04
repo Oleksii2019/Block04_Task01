9126689cc3a5d829022f1748d817e2a65cd9f1be
Oleksii2019 <Oleksii2019@users.noreply.github.com>
2019-06-04 10:07:20 +0300

    Начало

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -1,4 +1,18 @@
 package manager;
 
+import java.util.Scanner;
+
 public class Manager {
+    Scanner scanner;
+
+    Manager() {
+        scanner = new Scanner();
+
+    }
+
+    private void stopScanner() {
+        scanner.close();
+    }
+
+
 }
