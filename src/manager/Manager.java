e82b7e066a5244c5a8c9c092475e0307ee14b223
Oleksii2019 <Oleksii2019@users.noreply.github.com>
2019-06-08 22:30:37 +0300

    На 08.06 2

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -16,22 +16,19 @@ import java.util.Scanner;
 public class Manager {
 
     // Для временного хранения и передачи строковых данных между методами
-    private StringBuffer resultAfterCheck, currentRegex;
+    private StringBuffer currentRegex;
 
     private View view;
     private Model model;
     private Scanner scanner;
-
-    // Признак этапа выбора языка.
-    // Введено для использования одних методоа на разных этапах выполнения
-    private boolean choiceLanguageStage;
+    private ScanтerController sc;
 
     public Manager(View view, Model model) {
         scanner = new Scanner(System.in);
         this.view = view;
         this.model = model;
-        resultAfterCheck = new StringBuffer(Model.MAX_ENTRY_NAME_LENGTH);
         currentRegex = new StringBuffer(View.NAME_REGEX_UA.length());
+        sc = new ScanтerController(scanner, view);
     }
 
     private void stopScanner() {
@@ -45,25 +42,21 @@ public class Manager {
      */
     public void runManager() {
         choiceLanguage();
-        fillingFields(model.noteBook);
+        addRecord(model.noteBook);
         view.printNoteBookFields(model.noteBook,
                                  model.noteBook.subscriberList.size() - 1);
         stopScanner();
     }
 
-    private void fillingFields(NoteBook noteBook) {
-        String[] str = new String[Subscriber.MAX_ENTRY_NAME_AMOUNT];
-        for (int i = 0; i < Subscriber.MAX_ENTRY_NAME_AMOUNT; i++) {
-            getName(i);
-            str[i] = resultAfterCheck.toString();
-        }
-        noteBook.addSubscriber(new Subscriber(str));
-    }
-
     private void choiceLanguage() {
-        choiceLanguageStage = true;
-        while (!checkScannerLine(getScannerLine(0)));
-        if (resultAfterCheck.toString().equals(View.CHOICE_EN)) {
+        if (sc.getFromScanner(View.PROMPT_MESSAGE
+                + View.CHOICE_EN  + " - english, "
+                + View.CHOICE_UA + " - ukrainian)"
+                + View.COLON_SIGN,
+                View.NOT_OK_MESSAGE + View.NEW_LINE_SIGN,
+                View.CHOICE_LOCAL_REGEX,
+                View.NAME_REGEX_EN.length())
+                .equals(View.CHOICE_EN)) {
             view.setRegion(View.ENGLAND);
             currentRegex.replace(0, View.NAME_REGEX_UA.length(),
                                  View.NAME_REGEX_EN);
@@ -72,45 +65,26 @@ public class Manager {
             currentRegex.replace(0, View.NAME_REGEX_UA.length(),
                                  View.NAME_REGEX_UA);
         }
-        choiceLanguageStage = false;
     }
 
-    private void getName(int counter) {
-        while (!checkScannerLine(getScannerLine(counter)));
-    }
-
-    private String getScannerLine(int counter) {
-        if (choiceLanguageStage) {
-            view.printMessage(View.PROMPT_MESSAGE
-                    + View.CHOICE_EN  + " - english, "
-                    + View.CHOICE_UA + " - ukrainian)"
-                    + View.COLON_SIGN);
-        } else {
-            view.printBundleMessage(View.PROMPT_KEY);
-            view.printBundleMessage(Subscriber.FIELD_NAME[counter]);
-            view.printBundleMessage(View.COLON_SIGN_KEY);
+    private void addRecord(NoteBook noteBook) {
+        String[] str = new String[NoteBook.MAX_ENTRY_NAME_AMOUNT];
+        for (int i = 0; i < NoteBook.MAX_ENTRY_NAME_AMOUNT; i++) {
+            str[i] = getValue(i);
         }
-        return scanner.nextLine();
+        noteBook.addSubscriber(new Subscriber(str));
     }
 
-    private boolean checkScannerLine(String str) {
-        boolean res;
-        if (choiceLanguageStage) {
-            res = str.matches(View.CHOICE_LOCAL_REGEX);
-            resultAfterCheck.replace(0, Model.MAX_ENTRY_NAME_LENGTH, str);
-            if (!res) {
-                view.printMessage(View.NOT_OK_MESSAGE + View.NEW_LINE_SIGN);
-            }
-        } else {
-            res = str.matches(currentRegex.toString()) && (str.length()
-                              < Model.MAX_ENTRY_NAME_LENGTH);
-            resultAfterCheck.replace(0, Model.MAX_ENTRY_NAME_LENGTH, str);
-            if (!res) {
-                view.printBundleMessage(View.NOT_OK_MESSAGE_KEY);
-                view.printMessage(String.valueOf(View.NEW_LINE_SIGN));
-            }
-        }
-        return res;
+    private String getValue(int number) {
+        return sc.getFromScanner(
+                view.localMessenger.getString(View.PROMPT_KEY)
+                + view.localMessenger.getString(NoteBook
+                                                .FIELD_NAME[number])
+                + view.localMessenger.getString(View.COLON_SIGN_KEY),
+                view.localMessenger.getString(View.NOT_OK_MESSAGE_KEY)
+                + String.valueOf(View.NEW_LINE_SIGN),
+                currentRegex.toString(),
+                Model.MAX_ENTRY_NAME_LENGTH);
     }
 
 }
--- src/view/View.java
+++ src/view/View.java
@@ -14,18 +14,20 @@ import java.util.ResourceBundle;
  * @author Aleksey Muratov
  */
 public class View {
-    // Регулярное выражение, применяемое для контроля введенных имен,
-    // фамилмй, отчеств на английском языке
     public static String CHOICE_EN = "1";
     public static String CHOICE_UA = "2";
+
+    // Регулярное выражение, применяемое для контроля введенных имен,
+    // фамилмй, отчеств на английском языке
+    public static String CHOICE_LOCAL_REGEX = "[" + CHOICE_EN + CHOICE_UA
+            + "]{1}";
     public static String NAME_REGEX_EN = "^[A-Z]{1}[a-z]+[-]?[A-Z]?"
             + "{1}[a-z]+?$";
     public static String NAME_REGEX_UA = "^[А-ЯЇІЄҐ&&[^ъЫЭ]]{1}[а-яїі"
             + "єґ'&&[^ъыэ]]+-?[А-ЯЇІЄҐ&&[^ъЫЭ]]?[а-яїієґ'&&[^ъыэ]]+?$";
 //        NAME_REGEX_RU = "^([А-ЯЁ&&[^ЪЬ]]{1}[а-яё]+)|([А-ЯЁ&&[^ЪЬ]]"
 //                        + "{1}[а-яё]+-[А-ЯЁ&&[^ЪЬ]]{1}[а-яё]+)$";
-    public static String CHOICE_LOCAL_REGEX = "[" + CHOICE_EN + CHOICE_UA
-            + "]{1}";
+
     public static String MESSAGES_FILE_NAME = "messages";
     public static String PROMPT_KEY = "first.prompt";
     public static String COLON_SIGN_KEY = "second.prompt";
@@ -33,10 +35,9 @@ public class View {
     public static String NOT_OK_MESSAGE_KEY = "wrong.message";
     public static String ENGLAND = "en";
     public static String UKRAINE = "ua";
-    public static String PROMPT_MESSAGE = "Enter language (";
+    public static String PROMPT_MESSAGE = "Select language (";
     public static String COLON_SIGN = ":";
-    public static String NOT_OK_MESSAGE
-            = "It is wrong. Try again...";
+    public static String NOT_OK_MESSAGE = "It is wrong. Try again...";
     public static final char NEW_LINE_SIGN = '\n';
     public ResourceBundle localMessenger;
 
@@ -45,18 +46,13 @@ public class View {
     }
 
     public void setRegion(String language) {
-        localMessenger = ResourceBundle.getBundle(MESSAGES_FILE_NAME,
-                                                 new Locale(language));
-    }
-
-    public void printBundleMessage(String key) {
-        System.out.print(localMessenger.getString(key));
-
+        localMessenger = ResourceBundle.getBundle(
+                MESSAGES_FILE_NAME, new Locale(language));
     }
 
     public void printNoteBookFields(NoteBook noteBook, int number) {
         printMessage(String.valueOf(NEW_LINE_SIGN));
-        printBundleMessage(RESULT_KEY);
+        printMessage(localMessenger.getString(RESULT_KEY));
         printMessage(String.valueOf(NEW_LINE_SIGN));
         printMessage(noteBook.getFamilyName(number)
                      + NEW_LINE_SIGN);
